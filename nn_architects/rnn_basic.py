032d9341a1845b0bff9a52b80156f7540218a8d8
rodmarq <43847610+rodmarq@users.noreply.github.com>
2019-09-14 09:50:00 +0100

    added RNN backpropagation code
    Signed-off-by: rodmarq <43847610+rodmarq@users.noreply.github.com>

--- nn_architects/rnn_basic.py
+++ nn_architects/rnn_basic.py
@@ -125,9 +125,82 @@ def rnn_cell_backpropagation(da_next, cell_cache):
     ba = parameters["ba"]
     by = parameters["by"]
 
-    
+    # Computation of gradients with respect to the parameters
+    # The general expressions to compute the derivatives of the cost function with respect to the parameters are
+    # from the chain rule. These expressions are summarized as follows:
+    # - dWax: dJ/dWax = dJ/da_next * da_next/dz * dz/dWax
+    # - dWaa: dJ/dWaa = dJ/da_next * da_next/dz * dz/dWaa
+    # - dba: dJ/db = dJ/da_next * da_next/dz * dz/dba
+    # where:
+    # a = tanh(z), da = (1 - tanh(z)**2)*dz
+    # z = Wax*x + Waa*a_prev + ba
 
-    pass
+    dz = (1 - a_next ** 2) * da_next
+    dxt = np.dot(Wax.T, dz)
+    dWax = np.dot(dz, xt.T)
+    da_prev = np.dot(Waa.T, dz)
+    dWaa = np.dot(dz, a_prev.T)
+    dba = np.sum(dz, keepdims=True, axis=-1)
+
+    # Store gradients in a python dictionary
+    gradients = {"dxt": dxt, "da_prev": da_prev, "dWax": dWax, "dWaa": dWaa, "dba": dba}
+
+    return gradients
+
+def rnn_backpropagation(da, cache):
+    """
+    Implementation of the backward pass for a RNN over an entire sequence of input data.
+
+    Arguments:
+    da -- Upstream gradients of all hidden states, of shape (n_a, m, T_x)
+    caches -- tuple containing information from the forward pass (rnn_forward)
+
+    Returns:
+    gradients -- python dictionary containing:
+        dx -- Gradient with respect to the input data, numpy-array of shape (n_x, m, T_x)
+        da0 -- Gradient with respect to the initial hidden state, numpy-array of shape (n_a, m)
+        dWax -- Gradient with respect to the input's weight matrix, numpy-array of shape (n_a, n_x)
+        dWaa -- Gradient with respect to the hidden state's weight matrix, numpy-arrayof shape (n_a, n_a)
+    """
+
+    # Retrieve cache
+    (cache_t, x) = cache
+
+    # Get cache values for the first time step
+    (a1, a0, x1, parameters) = cache_t[0]
+
+    # Compute variable dimensions
+    n_a, m, T_x = da.shape
+    n_x, m = x1.shape
+
+    # Initialize gradients
+    dx = np.zeros((n_x, m, T_x))
+    dWax = np.zeros((n_a, n_x))
+    dWaa = np.zeros((n_a, n_a))
+    dba = np.zeros((n_a, 1))
+    da0 = np.zeros((n_a, m))
+    da_prev_t = np.zeros((n_a, m))
+
+    # Loop over all time-steps
+    for t in reversed((range(T_x))):
+        # Compute gradients for time step t
+        gradients = rnn_cell_backpropagation(da[:, :, t] + da_prev_t, cache_t[t])
+        dxt, da_prev_t, dWaxt, dWaat, dbat = gradients["dxt"], gradients["da_prev"], gradients["dWax"], \
+                                            gradients["dWaa"], gradients["dba"]
+
+        # Increment global derivatives with respect to parameters by adding their derivative at time-step t
+        dx[:, :, t] = dxt
+        dWax += dWaxt
+        dWaa += dWaat
+        dba += dbat
+
+    # Set da0 to the gradient of a which has been backpropagated through all time-steps
+    da0 = da_prev_t
+
+    # Store gradients in a dictionary
+    gradients = {"dx": dx, "da0": da0, "dWax": dWax, "dWaa": dWaa, "dba": dba}
+
+    return gradients
 
 
 # TESTS
